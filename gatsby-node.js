946dbd999228cb6ab8008b6dad1db7b2bfcb5361
Ashvin Motye <22303668+ashvinmotye@users.noreply.github.com>
2021-08-11 20:20:40 +0400

    [Missing Fields] - Create project default structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,44 @@
  */
 
 // You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = path.resolve("src/templates/markdown-page.js")
+
+  const result = await graphql(`
+    {
+      postsRemark: allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const posts = result.data.postsRemark.edges
+
+  // Create post detail pages
+  posts.forEach(({ node }) => {
+    createPage({
+      context: {
+        id: node.id,
+      },
+      path: node.frontmatter.slug,
+      component: blogPostTemplate,
+    })
+  })
+}
